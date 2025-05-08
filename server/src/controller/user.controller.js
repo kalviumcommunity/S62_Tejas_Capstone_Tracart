3eda27f9c7741b0522d5383dae675b5785c77802
Tej <jupitersorbeet@gmail.com>
2025-05-08 16:31:56 +0530

    post api added for users

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -2,6 +2,34 @@ import User from "../models/userModel.js";
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
+
+  try {
+    if (
+      !name ||
+      name.trim() == "" ||
+      !email ||
+      email.trim() == "" ||
+      !password ||
+      password.trim() == ""
+    ) {
+      res
+        .status(400)
+        .json({ message: "Name should not be empty in request body." });
+    }
+    const checkIfUSerPresent = await User.findOne({ email: email });
+
+    if (checkIfUSerPresent) {
+      return res.status(400).send("User already present");
+    }
+
+    const newUser = await new User({ name, email, password });
+    newUser.save();
+    return res
+      .status(201)
+      .json({ message: "user created successfully.", data: newUser });
+  } catch (error) {
+    res.send(500).json({ error: error.message });
+  }
 };
 
 const fetchAllUsers = async (req, res) => {
