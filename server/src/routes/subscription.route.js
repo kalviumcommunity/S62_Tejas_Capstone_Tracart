49c73573ffb70f7f3f5ec93dd09a683d64bd54ce
Tej <jupitersorbeet@gmail.com>
2025-05-16 00:02:52 +0530

    feat: added delete subscription

--- server/src/routes/subscription.route.js
+++ server/src/routes/subscription.route.js
@@ -4,11 +4,13 @@ import {
   createSubscription,
   fetchSubscriptions,
   updateSubscription,
+  deleteSubscription,
 } from "../controller/subscription.controller.js";
 
 router.get("/", fetchSubscriptions);
 router.post("/", createSubscription);
 // router.get("/:subscriptionId");
 router.put("/:id", updateSubscription);
+router.delete("/:id", deleteSubscription);
 
 export default router;
